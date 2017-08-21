28c3035e8f2ee9a41de5ef90712e695b75e01909
Rico Picone <ricopicone@gmail.com>
2017-08-21 16:36:41 -0700

    working well

--- courses_2017.py
+++ courses_2017.py
@@ -1,74 +1,88 @@
 # math and science
-new_course('CHM 145+L','1a')
-new_course('MTH 171','1a')
-new_course('MTH 172','1b')
-courses['MTH 172'].add_prereq('MTH 171')
-new_course('MTH 271','2b')
-courses['MTH 271'].add_prereq('MTH 172')
-new_course('MTH 322','2a')
-courses['MTH 322'].add_prereq('MTH 172')
-new_course('MTH 353','2b')
-courses['MTH 353'].add_prereq('MTH 172')
-new_course('PHY 171+L','1a')
-courses['PHY 171+L'].add_coprereq('MTH 171')
-new_course('PHY 172+L','1b')
-courses['PHY 172+L'].add_coprereq('MTH 172')
+new_course('MTH 171',4,'1F',False)
+new_course('PHY 171',4,'1F',False) \
+	.add_coprereq('MTH 171')
+new_course('PHY 171L',1,'1F',False) \
+	.add_coreq('PHY 171')
+new_course('MTH 172',4,'1S',False) \
+	.add_prereq('MTH 171')
+new_course('PHY 172',4,'1S',False) \
+	.add_coprereq('MTH 172')
+new_course('PHY 172L',1,'1S',False) \
+	.add_coreq('PHY 172')
+new_course('MTH 271',3,'2F',False) \
+	.add_prereq('MTH 172')
+new_course('MTH 322',3,'2F',False) \
+	.add_prereq('MTH 172')
+new_course('CHM 145',4,'2F',False)
+new_course('CHM 145L',1,'2F',False) \
+	.add_coprereq('CHM 145')
+new_course('MTH 353',3,'2S',False) \
+	.add_prereq('MTH 172')
 
 # GE
-new_course('GE 104','1b')
-courses['GE 104'].add_prereq('MTH 171')
-courses['GE 104'].add_prereq('PHY 171+L')
-new_course('GE 204','2a')
-courses['GE 204'].add_prereq('MTH 171')
-courses['GE 204'].add_prereq('PHY 171+L')
-new_course('GE 205','2b')
-courses['GE 205'].add_prereq('GE 204')
-new_course('GE 206+7','2b')
-courses['GE 206+7'].add_prereq('GE 204')
-new_course('GE 359','4b')
+new_course('GE 104',3,'1S',False) \
+	.add_prereq('MTH 171') \
+	.add_prereq('PHY 171')
+new_course('GE 204',3,'2F',False) \
+	.add_prereq('MTH 171') \
+	.add_prereq('PHY 171')
+new_course('GE 205',3,'2S',False) \
+	.add_prereq('GE 204')
+new_course('GE 206',3,'2S',False) \
+	.add_prereq('GE 204')
+new_course('GE 207',1,'2S',False) \
+	.add_coprereq('GE 206')
+new_course('GE 359',3,'4S')
 
 # ME
-new_course('ME 300+L','3a')
-courses['ME 300+L'].add_prereq('GE 206+7')
-new_course('ME 302','3b')
-courses['ME 302'].add_prereq('GE 206+7')
-new_course('ME 303','3a')
-courses['ME 303'].add_prereq('GE 206+7')
-courses['ME 303'].add_prereq('CHM 145+L')
-new_course('ME 308+9','3b')
-courses['ME 308+9'].add_prereq('GE 205')
-courses['ME 308+9'].add_prereq('GE 206+7')
-new_course('ME 315','3a')
-courses['ME 315'].add_prereq('PHY 172+L')
-new_course('ME 316','3a')
-courses['ME 316'].add_coprereq('ME 315')
-courses['ME 316'].add_coprereq('ME 345')
-new_course('ME 340','3a')
-courses['ME 340'].add_prereq('CHM 145+L')
-courses['ME 340'].add_prereq('PHY 172+L')
-courses['ME 340'].add_coprereq('MTH 322')
-new_course('ME 345','3a')
-courses['ME 345'].add_prereq('MTH 322')
-courses['ME 345'].add_prereq('PHY 172+L')
-new_course('ME 350','3b')
-courses['ME 350'].add_prereq('GE 104')
-new_course('ME 370','3b')
-courses['ME 370'].add_prereq('MTH 322')
-courses['ME 370'].add_prereq('GE 205')
-courses['ME 370'].add_coprereq('MTH 353')
-new_course('ME 410','4a')
-courses['ME 410'].add_prereq('GE 205')
-courses['ME 410'].add_prereq('MTH 322')
-new_course('ME 430+L','4a')
-courses['ME 430+L'].add_prereq('ME 340')
-courses['ME 430+L'].add_prereq('ME 308+9')
-courses['ME 430+L'].add_prereq('MTH 322')
-new_course('ME 498','4a')
-courses['ME 498'].add_prereq('ME 302')
-courses['ME 498'].add_prereq('ME 370')
-courses['ME 498'].add_coprereq('ME 430+L')
-new_course('ME 499+L','4b')
-courses['ME 499+L'].add_prereq('ME 498')
-new_course('ME Elective I','4a')
-new_course('ME Elective II','4b')
-new_course('ME Elective III','4b')
+new_course('ME 300',3,'3F',False) \
+	.add_prereq('GE 206')
+new_course('ME 300L',1,'3F',False) \
+	.add_coreq('ME 300')
+new_course('ME 303',3,'3F',False) \
+	.add_prereq('GE 206') \
+	.add_prereq('CHM 145')
+new_course('ME 315',3,'3F',False) \
+	.add_prereq('PHY 172')
+new_course('ME 316',1,'3F',False) \
+	.add_coprereq('ME 315') \
+	.add_coprereq('ME 345')
+new_course('ME 340',3,'3F',False) \
+	.add_prereq('CHM 145') \
+	.add_prereq('PHY 172') \
+	.add_coprereq('MTH 322')
+new_course('ME 345',3,'3F',False) \
+	.add_prereq('MTH 322') \
+	.add_prereq('PHY 172')
+new_course('ME 302',3,'3S',False) \
+	.add_prereq('GE 206')
+new_course('ME 308',3,'3S',False) \
+	.add_prereq('GE 205') \
+	.add_prereq('GE 206')
+new_course('ME 309',1,'3S',False) \
+	.add_coreq('ME 308')
+new_course('ME 350',3,'3S',False) \
+	.add_prereq('GE 104')
+new_course('ME 370',3,'3S',False) \
+	.add_prereq('ME 345') \
+	.add_prereq('GE 205') \
+	.add_coprereq('MTH 353')
+new_course('ME 410',3,'4F',False) \
+	.add_prereq('GE 205') \
+	.add_prereq('MTH 322')
+new_course('ME 430',3,'4F',False) \
+	.add_prereq('ME 340') \
+	.add_prereq('ME 308') \
+	.add_prereq('MTH 322')
+new_course('ME 430L',1,'4F',False) \
+	.add_coreq('ME 430')
+new_course('ME 498',3,'4F',False) \
+	.add_prereq('ME 302') \
+	.add_prereq('ME 370') \
+	.add_coprereq('ME 430')
+new_course('ME 499',3,'4S',False) \
+	.add_prereq('ME 498')
+new_course('ME El. 1',3,'4F',False)
+new_course('ME El. 2',3,'4S',False)
+new_course('ME El. 3',3,'4S',False)
--- flowchart_generator.py
+++ flowchart_generator.py
@@ -1,7 +1,11 @@
+#!/usr/local/bin/python
+
 import sys
 import graphviz as gv
 import functools
 from pprint import pprint
+import random
+import math
 
 #### functions and classes ####
 
@@ -18,11 +22,16 @@ def apply_styles(graph, styles):
   return graph
 
 def add_nodes(graph, nodes):
+  global courses
   for n in nodes:
     if isinstance(n, tuple):
-      graph.node(n[0], **n[1])
+      graph.node(courses[n[0]].name+' '+str(courses[n[0]].credits), **n[1])
     else:
-      graph.node(n)
+      if courses[n].completed:
+        graph.attr('node', fillcolor='#cecdc9', color='#cecdc9')
+      else:
+        graph.attr('node', fillcolor='white', color='white')
+      graph.node(courses[n].name,'<<font color="#6e6d6a"><sub>'+str(courses[n].credits)+'</sub></font>   '+courses[n].name+'>')
   return graph
 
 def add_edges(graph, edges):
@@ -36,9 +45,11 @@ def add_edges(graph, edges):
 class course:
   """Defines a course"""
 
-  def __init__(self, name, term):
+  def __init__(self, name, credits, term, completed=False):
     self.name = name
+    self.credits = credits
     self.term = term
+    self.completed = completed
     self.prereqs = []
     self.coreqs = []
     self.coprereqs = []
@@ -46,21 +57,53 @@ class course:
 
   def add_prereq(self, prereq):
     self.prereqs.append(prereq)
+    return self
 
   def add_coreq(self, coreq):
     self.coreqs.append(coreq)
+    return self
 
   def add_coprereq(self, coprereq):
     self.coprereqs.append(coprereq)
+    return self
 
   def add_style(self, style_key, style_value):
     self.styles.update({style_key: style_value})
+    return self
 
-def new_course(name,term):
+def new_course(name,credits,term,completed=False):
   global courses
   try: courses
   except NameError: courses = {}
-  courses.update({name: course(name,term)})
+  courses.update({name: course(name,credits,term,completed)})
+  return courses[name]
+
+#### colors ####
+
+kelly_colors_hex = [
+  0xFFB300, # Vivid Yellow
+  0x803E75, # Strong Purple
+  0xFF6800, # Vivid Orange
+  0xA6BDD7, # Very Light Blue
+  0xC10020, # Vivid Red
+  0xCEA262, # Grayish Yellow
+  0x817066, # Medium Gray
+
+  # The following don't work well for people with defective color vision
+  0x007D34, # Vivid Green
+  0xF6768E, # Strong Purplish Pink
+  0x00538A, # Strong Blue
+  0xFF7A5C, # Strong Yellowish Pink
+  0x53377A, # Strong Violet
+  0xFF8E00, # Vivid Orange Yellow
+  0xB32851, # Strong Purplish Red
+  0xF4C800, # Vivid Greenish Yellow
+  0x7F180D, # Strong Reddish Brown
+  0x93AA00, # Vivid Yellowish Green
+  0x593315, # Deep Yellowish Brown
+  0xF13A13, # Vivid Reddish Orange
+  0x232C16, # Dark Olive Green
+  ]
 
 #### build the graph ####
 
@@ -70,29 +113,60 @@ digraph = functools.partial(gv.Digraph, format='svg')
 execfile(sys.argv[1])
 
 nodes_by_term = [[],[],[],[],[],[],[],[]] # initialize
-terms_by_name = ['1a','1b','2a','2b','3a','3b','4a','4b']
+terms_by_name = ['1F','1S','2F','2S','3F','3S','4F','4S']
 for course_key in courses.keys():
   course_term_index = terms_by_name.index(courses[course_key].term)
   nodes_by_term[course_term_index].append(courses[course_key].name)
 
 edges = [] # initialize
 edges_cluster = [] # initialize
+color_lookup = {} # initialize
+i_mod = 0
 for course_key in courses.keys():
   for prereq in courses[course_key].prereqs:
-    edges.append(
-      ((prereq,courses[course_key].name),{'style': 'solid'})
-    )
+    if prereq in color_lookup:
+      edge_color = color_lookup[prereq]
+    else:
+      i_mod = (i_mod+1)%len(kelly_colors_hex)
+      edge_color = str("#%06x" % kelly_colors_hex[i_mod])
+      color_lookup[prereq] = edge_color
+    if not courses[prereq].completed:
+      edges.append(
+        ((prereq,courses[course_key].name),{'style': 'solid','color': edge_color})
+      )
+  for coreq in courses[course_key].coreqs:
+    if coreq in color_lookup:
+      edge_color = color_lookup[coreq]
+    else:
+      i_mod = (i_mod+1)%len(kelly_colors_hex)
+      edge_color = str("#%06x" % kelly_colors_hex[i_mod])
+      color_lookup[coreq] = edge_color
+    if not courses[coreq].completed:
+      edges.append(
+        ((coreq,courses[course_key].name),{'style': 'solid','color': edge_color,'dir': 'both'})
+      )
   for coprereq in courses[course_key].coprereqs:
-    edges.append(
-      ((coprereq,courses[course_key].name),{'style': 'dashed'})
-    )
+    if coprereq in color_lookup:
+      edge_color = color_lookup[coprereq]
+    else:
+      i_mod = (i_mod+1)%len(kelly_colors_hex)
+      edge_color = str("#%06x" % kelly_colors_hex[i_mod])
+      color_lookup[coprereq] = edge_color
+    if not courses[coprereq].completed:
+      edges.append(
+        ((coprereq,courses[course_key].name),{'style': 'dashed','color': edge_color})
+      )
 
-g = digraph()
+g = digraph(engine='dot')
 g.attr(newrank='true',compound='true')
 for i,term in enumerate(terms_by_name):
+  term_credits = 0
+  for c in nodes_by_term[i]:
+    term_credits += courses[c].credits
   name_now = 'cluster_'+str(i)
+  label_cluster = {'label': '<<B>term: '+str(terms_by_name[i])+'</B><BR/><FONT POINT-SIZE="10" COLOR="#6e6d6a">'+str(term_credits)+' cr</FONT>>'}
   h = add_nodes(digraph(name_now), nodes_by_term[i])
-  h.graph_attr.update(rank='same',color='black')
+  h.graph_attr.update(rank='same',color='#cecdc9',style='filled',**label_cluster)
   g.subgraph(h)
   if i > 0:
     edges_cluster.append(
@@ -105,34 +179,73 @@ for i,term in enumerate(terms_by_name):
 g = add_edges(g, edges_cluster)
 g = add_edges(g, edges)
 
+font = 'Helvetica'
+node_color = '#5e5d87'
 styles = {
   'graph': {
     # 'label': 'Prerequisite structure',
     'fontsize': '16',
     'fontcolor': 'black',
-    'fontname': 'Courier',
+    'fontname': font,
     'bgcolor': 'white',
-    'rankdir': 'TB',
+    'rankdir': 'LR',
   },
   'nodes': {
-    'fontname': 'Courier',
+    'fontname': font,
     'shape': 'rectangle',
-    'fontcolor': 'white',
+    'fontcolor': node_color,
     'color': 'white',
     'style': 'filled',
-    'fillcolor': '#006699',
+    'fillcolor': 'white',
   },
   'edges': {
     'style': 'dashed',
     'color': 'black',
-    'arrowhead': 'open',
-    'fontname': 'Courier',
+    'fontname': font,
     'fontsize': '12',
     'fontcolor': 'black',
+    'penwidth': '3',
+    'arrowsize': '1',
+    'arrowhead': 'normal',
   }
 }
 
 
 g = apply_styles(g, styles)
-g.render('img/g')
+g.render('img/'+sys.argv[1].split('.')[0])
+g.view()
 
+#### legend ####
+# gl = digraph(engine='dot')
+# gl.node('pr','pre-requisite')
+# gl.node('cr','co-requisite')
+# gl.node('pcr','pre- or co-requisite')
+# gl.edge()
+
+from graphviz import Source
+temp = """
+digraph {
+  rankdir=LR
+  node [shape=plaintext]
+  fontname=Helvetica
+  subgraph cluster_01 {
+    label=Legend
+    key [label=<<table border="0" cellpadding="2" cellspacing="0" cellborder="0">
+      <tr><td align="right" port="i1">prerequisite  </td></tr>
+      <tr><td align="right" port="i2">corequisite  </td></tr>
+      <tr><td align="right" port="i3">con-prerequisite  </td></tr>
+      </table>>, fontname=Helvetica]
+    key2 [label=<<table border="0" cellpadding="2" cellspacing="0" cellborder="0">
+      <tr><td port="i1">&nbsp;</td></tr>
+      <tr><td port="i2">&nbsp;</td></tr>
+      <tr><td port="i3">&nbsp;</td></tr>
+      </table>>, fontname=Helvetica]
+    key:i1:e -> key2:i1:w [penwidth=3]
+    key:i2:e -> key2:i2:w [dir="both",penwidth=3]
+    key:i3:e -> key2:i3:w [style=dashed,penwidth=3]
+  }
+}
+"""
+gl = Source(temp, filename="img/legend", format="svg")
+gl.render('img/'+sys.argv[1].split('.')[0]+'_legend')
+# gl.view()
